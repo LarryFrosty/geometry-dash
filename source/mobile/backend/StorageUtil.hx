7a3ac81e23ad6a96eacb976b1e86559f8113ceb1
LarryFrosty <larrydobefrosty@gmail.com>
2024-09-07 07:32:55 +0800

    Change external name and default storage type

--- source/mobile/backend/StorageUtil.hx
+++ source/mobile/backend/StorageUtil.hx
@@ -138,7 +138,7 @@ enum abstract StorageType(String) from String to String
 {
 	final forcedPath = '/storage/emulated/0/';
 	final packageNameLocal = 'com.shadowmario.psychengine';
-	final fileLocal = 'PsychEngine';
+	final fileLocal = 'PsychEngineModified';
 
 	var EXTERNAL_DATA = "EXTERNAL_DATA";
 	var EXTERNAL_OBB = "EXTERNAL_OBB";
