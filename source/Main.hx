001d5c106c23e901dc1a8805195bb67683adadca
LarryFrosty <larrydobefrosty@gmail.com>
2024-09-07 09:06:38 +0800

    :(

--- source/Main.hx
+++ source/Main.hx
@@ -173,7 +173,7 @@ class Main extends Sprite
 
 		#if HSCRIPT_ALLOWED
 		crowplexus.iris.Iris.logLevel = function(level: crowplexus.iris.ErrorSeverity, x, ?pos: haxe.PosInfos): Void {
-			if (PlayState.instance != null) PlayState.instance.addTextToDebug("ERROR: $x", FlxColor.RED);
+			if (PlayState.instance != null) PlayState.instance.addTextToDebug("ERROR: "+Std.string(x), FlxColor.RED);
 		}
 		#end
 
