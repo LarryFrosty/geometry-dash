8fd5c0efeb833da842b22cecbd32cb62a2ee3846
LarryFrosty <larrydobefrosty@gmail.com>
2024-10-14 22:30:23 +0800

    nvm

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -39,7 +39,7 @@ class MainMenuState extends MusicBeatState
 
 		persistentUpdate = persistentDraw = true;
 
-		var yScroll:Float = 0.25;
+		var yScroll:Float = Math.max(0.25 - (0.05 * (optionShit.length - 4)), 0.1);
 		var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('menuBG'));
 		bg.antialiasing = ClientPrefs.data.antialiasing;
 		bg.scrollFactor.set(0, yScroll);
